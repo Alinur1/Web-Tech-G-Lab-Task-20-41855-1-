cbcefc30cb1fa0173f71402e6ad99f6662bb44d2
Alinur1 <87419413+Alinur1@users.noreply.github.com>
2022-06-10 23:35:20 +0600

    Lab task updated

--- Lab_Task-2/PHP form Validation.php
+++ Lab_Task-2/PHP form Validation.php
@@ -8,8 +8,8 @@
 </head>
 <body>
     <?php
-        $name=$email=$dob=$Gender=$degree=$blood="";
-        $nameErr=$emailErr=$dobErr=$GenderErr=$degreeErr=$bloodErr="";
+        $name=$email=$dob=$gender=$degree=$blood="";
+        $nameErr=$emailErr=$dobErr=$genderErr=$degreeErr=$bloodErr="";
 
         if($_SERVER["REQUEST_METHOD"] == "POST")
         {
@@ -20,7 +20,17 @@
             else
             {
                 $name = $_POST["name"];
+                if(!preg_match("/^[a-zA-Z- ]*$/",$name))
+                {
+                    $nameErr = "Only letters, periods and dash can be used.";
+                }
+
+                if(!str_word_count($name) < 2)
+                {
+                    $nameErr = "At least 2 words are required.";
+                }             
             }
+
 
             if(empty($_POST["email"]))
             {
@@ -29,6 +39,38 @@
             else
             {
                 $email = $_POST["email"];
+
+                if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+                {
+                    $emailErr = "Please enter a valid email.";
+                }
+            }
+
+            if(empty($_POST["dob"]))
+            {
+                $dobErr = "Date of Birth is required";
+            }
+            else
+            {
+                $dob = $_POST["dob"];
+            }
+
+            if(empty($_POST["gender"]))
+            {
+                $genderErr = "Gender should be selected.";
+            }
+            else
+            {
+                $gender = $_POST["gender"];
+            }
+
+            if(empty($_POST["degree"]))
+            {
+                $degreeErr = "At least one degree must be selected.";
+            }
+            else
+            {
+                $degree = $_POST["degree"];
             }
         }
 
@@ -46,7 +88,56 @@
     <span class="error">* <?php echo $emailErr;?></span>
     <br><br>
 
+    <label>Date of Birth: </label>
+    <input type="date" name="dob" value="<?php echo $dob;?>">
+    <span class="error">* <?php echo $dobErr;?></span>
+    <br><br>
+
+    <label>Select Gender: </label>
+    <input type="radio" name="gender" <?php if (isset($gender) && $gender=="Male") echo "checked";?> value="Male">Male
+    <input type="radio" name="gender" <?php if(isset($gender) && $gender=="Female") echo "checked";?> value="Female">Female
+    <input type="radio" name="gender" <?php if(isset($gender) && $gender=="Other") echo "checked";?> value="Other"> Other
+    <span class="error">*<?php echo $genderErr?></span>
+    <br><br>
+
+    
+
+    <label>Select a degree: </label>
+    <input type="checkbox" name="degree" <?php if(isset($degree) && $degree=="SSC") echo "checked";?> value="SSC">SSC
+    <input type="checkbox" name="degree" <?php if(isset($degree) && $degree=="HSC") echo "checked";?> value="HSC">HSC
+    <input type="checkbox" name="degree" <?php if(isset($degree) && $degree=="BSc") echo "checked";?> value="BSc">BSc
+    <input type="checkbox" name="degree" <?php if(isset($degree) && $degree=="MSc") echo "checked";?> value="MSc">MSc
+    <span class="error">*<?php echo $degreeErr?></span>
+    <br><br>
+
+
+    <label for="blood_group">Select your Blood Group: </label>
+    <select>
+        <option name="blood_group" <?php if(isset($blood) && $blood=="A+") echo "checked";?> value="A+">A+</option>
+        <option name="blood_group" <?php if(isset($blood) && $blood=="B+") echo "checked";?> value="B+">B+</option>
+        <option name="blood_group" <?php if(isset($blood) && $blood=="AB+") echo "checked";?> value="AB+">AB+</option>
+        <option name="blood_group" <?php if(isset($blood) && $blood=="O+") echo "checked";?> value="O+">O+</option>
+    </select>
+    <span class="error">*<?php echo $bloodErr?></span>
+    <br><br>
+
     <input type="submit" name="submit" value="Submit">
     </form>
+
+    <?php
+    echo "<h2>Your input</h2>";
+    echo "<b>Your name: </b>".$name;
+    echo "<br>";
+    echo "<b>Your email: </b>".$email;
+    echo "<br>";
+    echo "<b>Your Date of Birth: </b>".$dob;
+    echo "<br>";
+    echo "<b>Gender: </b>".$gender;
+    echo "<br>";
+    echo "<b>Degree you have achieved: </b>".$degree;
+    echo "<br>";
+    echo "<b>Your blood group: </b>".$blood;
+    echo "<br>";
+    ?>
 </body>
 </html>
