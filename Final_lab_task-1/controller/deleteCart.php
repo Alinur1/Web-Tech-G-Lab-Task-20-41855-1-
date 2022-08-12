e22b3de8a80f0d4528a9ab09f42242199e1835c0
Alinur1 <87419413+Alinur1@users.noreply.github.com>
2022-08-12 15:39:17 +0600

    Final_lab_Task-5 deleted due to update purpose

--- Final_lab_task-1/controller/deleteCart.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once '../model/model.php';
-
-if(deleteCart($_POST['foodID']))
-{
-    header('Location: ../showAllFood.php');
-}
-?>
--- Final_lab_task-1/nav.php
+++
@@ -1,5 +0,0 @@
-<ul>
-    <li><a href="addCart.php">Add To Cart</a></li>
-    <li><a href="showAllFood.php">Show all food</a></li>
-    <li><a href="searchFood.php">Search Food</a></li>
-</ul>
